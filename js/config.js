2facd2085d8e18b508ee329f0343474f07b940e7
Lciarallo <luizeduardociarallo@gmail.com>
2022-12-08 01:47:26 -0300

    08-12-2022

--- js/config.js
+++ js/config.js
@@ -100,7 +100,7 @@ const CONFIG = {
       category: 'Streaming',
       name: 'YouTube',
       key: 'y',
-      url: 'https://youtube.com',
+      url: 'https://redirect.invidious.io/',
       search: '/results?search_query={}',
       color: 'linear-gradient(135deg, #cd201f, #cd4c1f)',
       icon: 'youtube',
